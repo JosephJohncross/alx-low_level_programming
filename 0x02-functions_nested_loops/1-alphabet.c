2cfe4ae86998e131cd6186102619adbb07951106
Joseph Ibochi <josephibochi@gmail.com>
2023-06-20 12:38:11 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- *main  - Prints the alphabet in lowercase, followed by a new line
+ * print_alphabet_x10 - Prints the alphabet 10 times.
  *
  * Return: Always 0 (Success)
  */
-main(void)
+print_alphabet_x10(void)
 {
-	int i;
+	int a, b;
 
-	for (i = 97; i <= 122; i++)
+	for (a = 0; a <= 9; a++)
 	{
-		putchar(i);
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+	_putchar('\n');
 	}
-	putchar('\n');
 	return (0);
 }
