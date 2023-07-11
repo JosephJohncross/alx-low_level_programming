e55cf2e15ebed0ca0ee558ad78a6e531c0c0ef36
JosephJohncross <josephibochi@gmail.com>
2023-07-11 21:17:21 +0100

    Update to the malloc file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,50 +3,26 @@
 #include <stdlib.h>
 
 /**
- * str_concat - function that concatenates two strings.
- *              if NULL is passed, treat it as an empty string
- *              The function should return NULL on failure
- *
- * @s1: This is the output string
- * @s2: This is the input string
- *
- * Return: The returned pointer should point to a newly allocated space in
- *         memory which contains the contents of s1, followed by the contents 
- *         of s2, and null terminated
+ * create_array - Entry point
+ *@size: size of the array
+ *@c: character
+ * Return: a pointer to the array, or NULL if it fails
  */
-
-char *str_concat(char *s1, char *s2)
+char *create_array(unsigned int size, char c)
 {
-	int a, b;
-	char *s;
+	char *array = NULL;
+	unsigned int i;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	for (a = 0; s1[a] != '\0'; a++)
-	{
-	}
-	for (b = 0; s2[b] != '\0'; b++)
-	{
-	}
-
-	s = (char *)  malloc(((a + b) + 1) * sizeof(char));
-
-	if (s == NULL)
+	if (size == 0)
 		return (NULL);
-	for (a = 0; s1[a] != '\0'; a++)
+	if (size != 0)
 	{
-		s[a] = s1[a];
+		array = (char *)malloc(size * sizeof(char));
+		if (array != NULL)
+		{
+			for (i = 0; i < size; i++)
+				array[i] = c;
+		}
 	}
-	for (b = 0; s2[b] != '\0'; b++)
-	{
-		s[a] = s2[b];
-		a++;
-	}
-	return (s);
+	return (array);
 }
