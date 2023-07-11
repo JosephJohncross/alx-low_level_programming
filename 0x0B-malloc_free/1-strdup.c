fa3f8b214c14210ce576cfb0999d28dbd0b4a151
JosephJohncross <josephibochi@gmail.com>
2023-07-11 21:38:23 +0100

    updates

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,7 +7,7 @@
  *
  * @str: This is the input string
  *
- * Return: function returns a pointer to the duplicated string. It returns 
+ * Return: function returns a pointer to the duplicated string. It returns
  *         NULL if insufficient memory was available
  */
 char *_strdup(char *str)
@@ -20,12 +20,12 @@ char *_strdup(char *str)
 
 	for (a = 0; str[a] != '\0'; a++)
 		;
-	s = (char *)  malloc((a + 1) * sizeof(char));
+	s = (char *)malloc((a + 1) * sizeof(char));
 
 	if (s == NULL)
 		return (NULL);
 
-	for (b = 0; b < a ; b++)
+	for (b = 0; b < a; b++)
 		s[b] = str[b];
 
 	s[a + 1] = '\0';
