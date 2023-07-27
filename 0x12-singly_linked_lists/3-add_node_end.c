e92fcfcbecdcf9c83b6595f22566bc09d362df65
JosephJohncross <josephibochi@gmail.com>
2023-07-27 22:20:14 +0100

    initial commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -28,7 +28,7 @@ int len(const char *str)
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = (list_t *) malloc(sizeof(list_t));
+	list_t *new_node = (list_t *)malloc(sizeof(list_t));
 	list_t *last = *head;
 
 	if (new_node == NULL || str == NULL)
